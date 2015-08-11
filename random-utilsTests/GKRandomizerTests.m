4d168ab2ce09cfd248e884fd5ecddfd43b9c79dc
Grzegorz Krukowski <grkrukowski@gmail.com>
2015-08-11 23:41:48 +0200

    - added project files and tests

---
+++ random-utilsTests/GKRandomizerTests.m
@@ -0,0 +1,33 @@
+//
+//  GKRandomizerTests.m
+//  random-utils
+//
+//  Created by Grzegorz Krukowski on 11/08/15.
+//  Copyright (c) 2015 GrzegorzKrukowski. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface GKRandomizerTests : XCTestCase
+
+@end
+
+@implementation GKRandomizerTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+@end
